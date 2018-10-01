87cf65caf3d7ac0b850429ce5590d9d7d521696f
king1600 <3dsarcard@gmail.com>
2018-10-01 18:55:21 -0500

    glr: freestnding binary

--- glr/src/main.rs
+++ glr/src/main.rs
@@ -1,8 +1,10 @@
 #![no_std]
+#![no_main]
 
 extern crate libc;
 extern crate kernel32;
 
-fn main() {
-
+#[no_mangle]
+pub unsafe extern "C" fn main(_argc: i32, _argv: *const *const u8) -> i32 {
+    0
 }
